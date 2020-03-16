70289c0b72e47011850e646801f5e3e6b6933307
Arkadiusz Pachucy <arkadiusz.pachucy@payu.com>
2020-03-16 13:56:12 +0100

    Add in memory authentication

--- src/main/kotlin/unii/health/pressure/calculator/backend/pressurecalculatorbackend/configuration/SecurityConfig.kt
+++ src/main/kotlin/unii/health/pressure/calculator/backend/pressurecalculatorbackend/configuration/SecurityConfig.kt
@@ -1,45 +1,41 @@
-/*package unii.health.pressure.calculator.backend.pressurecalculatorbackend.configuration
+package unii.health.pressure.calculator.backend.pressurecalculatorbackend.configuration
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import unii.health.pressure.calculator.backend.pressurecalculatorbackend.app.filters.CsrfGrantingFilter
+import unii.health.pressure.calculator.backend.pressurecalculatorbackend.app.filters.CsrfValidationFilter
 
+import javax.servlet.Filter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
-
-    companion object {
-        const val FILTER_ALL: String = "/"
-        const val ENDPOINT_PATH: String = "/user"
-    }
-
     override fun configure(auth: AuthenticationManagerBuilder?) {
         auth?.inMemoryAuthentication()
                 ?.withUser("admin")
                 ?.password("{noop}admin")
-                ?.roles("Admin")
+                ?.roles("ADMIN")
                 ?.and()
                 ?.withUser("user")
                 ?.password("{noop}user")
-                ?.roles("User")
+                ?.roles("USER")
 
     }
-
 
     override fun configure(http: HttpSecurity?) {
-        http?.authorizeRequests()
-                ?.antMatchers(FILTER_ALL)
-                ?.permitAll()
-        /* ?.and()
-         ?.formLogin()*/
+        http?.csrf()
+                ?.disable()
+                ?.authorizeRequests()
+                ?.anyRequest()?.authenticated()
+                ?.and()
+                ?.httpBasic()
+
     }
-
-
-}*/
+}
